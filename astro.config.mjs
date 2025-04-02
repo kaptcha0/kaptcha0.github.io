02751d7210de5a391d12479ebaa2b1e928926de6
kaptcha0 <50426223+kaptcha0@users.noreply.github.com>
2025-04-01 21:29:30 -0400

    set up github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kaptcha0.github.io",
   integrations: [mdx()],
 
   vite: {
